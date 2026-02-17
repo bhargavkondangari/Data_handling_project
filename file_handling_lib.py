082a501253d85ec781d1133f64e4775b7589ac3b
unknown <bharbe862@gmail.com>
2026-02-17 09:08:04 +0530

    pyspark_intial

---
+++ file_handling_lib.py
@@ -0,0 +1,10 @@
+import psutil
+def print_hi(name):
+    print(f'Hello, {name}!')
+
+proc = psutil.process_iter()
+
+for process in proc:
+    cpu_percentages = process.cpu_percent()
+    memory_usage = process.memory_percent()
+    print(f"{cpu_percentages}, :::: {memory_usage}")
